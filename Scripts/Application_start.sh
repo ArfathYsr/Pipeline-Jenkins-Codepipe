9b8000a6a4977c516fa3634ed0dae3f5931371ce
Arfath <arfath.419@gmail.com>
2025-05-21 16:55:53 +0530

    application_start script modified

--- Scripts/Application_start.sh
+++ Scripts/Application_start.sh
@@ -2,12 +2,12 @@
 echo "Deploying React app to Apache2..."
 
 # Clear existing Apache root content
-sudo rm -rf /var/www/html/*
+rm -rf /var/www/html/*
 
 # Copy new build to Apache root
-sudo cp -r /home/ubuntu/react2/build/* /var/www/html/
+cp -r /home/ubuntu/react2/build/* /var/www/html/
 
 # Restart Apache
-sudo systemctl restart apache2
+systemctl restart apache2
 
 echo "Deployment complete. React app is live on Apache2."
